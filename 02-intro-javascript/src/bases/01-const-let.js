05246ed5ceaad92df19f5ad94701ac474f06ad69
WilliamDelCid <dm18019@ues.edu.sv>
2022-09-03 22:27:45 -0600

    Case

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+//Variables y Constantes
+
+const name = 'William';
+const lastName = 'Del cid'
+
+let valorDado = 5;
+
+console.log(name,lastName,valorDado);
+
+if(true){
+    const name = 'Mishel'
+    console.log(name)
+}
+console.log(valorDado)
